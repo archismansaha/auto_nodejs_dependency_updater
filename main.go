d08ec4d1faca13183dbe4fecdac28b3e233e784b
archisman saha <76175105+archismansaha@users.noreply.github.com>
2024-05-09 21:01:36 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 import "dependency_updater/colors"
@@ -11,10 +12,11 @@ import "dependency_updater/dependencies"
 
 func main() {
 	// Define command-line flags
-	ignoreDeps := flag.Bool("ignore-deps", false, "Ignore updating dependencies")
-	ignoreDevDeps := flag.Bool("ignore-dev-deps", false, "Ignore updating devDependencies")
+	ignoreDeps := flag.Bool("ignore-deps", false, "Ignore updating dependencies in package.json defined as {dependencies}")
+	ignoreDependencies := flag.String("ignore-dependencies", "", "Ignore updating dependencies comma(,) seperated")
+	ignoreDevDeps := flag.Bool("ignore-dev-deps", false, "Ignore updating devDependencies in package.json defined as {devDependencies}")
 	projectPath := flag.String("path", ".", "Path of the project")
- fmt.Printf("Project path", projectPath)
+ //fmt.Printf(`Project path %s %s %s`, projectPath,ignoreDeps,ignoreDevDeps,ignoreDependencies)
 	// Define usage message
 	flag.Usage = func() {
 		colors.PrintUsage()
@@ -28,17 +30,19 @@ func main() {
 	// Check if no flags are provided
 	if flag.NFlag() == 0 {
 		ignoreDeps := false
+		ignoreDependencies:=[]string{}
 		ignoreDevDeps := false
 		projectPath := "."
 
 		// Call update function with default values
-		if err := dependencies.Update(projectPath, ignoreDeps, ignoreDevDeps); err != nil {
+		if err := dependencies.Update(projectPath, ignoreDeps, ignoreDevDeps,ignoreDependencies); err != nil {
 			colors.PrintError("Error updating dependencies:", err.Error())
 			os.Exit(1)
 		}
 		return
 	}
-
+ignoreDependency:=strings.Split(*ignoreDependencies, ",")
+fmt.Printf(`Project path %s %s %s`, projectPath,ignoreDeps,ignoreDevDeps,ignoreDependency)
 	// Check for conflicting flags
 	if *ignoreDeps && *ignoreDevDeps {
 		colors.PrintError("Error: Cannot use both --ignore-dependencies and --ignore-devDependencies flags at the same time.")
@@ -47,7 +51,7 @@ func main() {
 	}
 
 	// Call update function with provided values
-	if err := dependencies.Update(*projectPath, *ignoreDeps, *ignoreDevDeps); err != nil {
+	if err := dependencies.Update(*projectPath, *ignoreDeps, *ignoreDevDeps,ignoreDependency); err != nil {
 		colors.PrintError("Error updating dependencies:", err.Error())
 		os.Exit(1)
 	}
